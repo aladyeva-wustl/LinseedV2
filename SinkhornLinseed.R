cb143480f499b6ff3d1e4923a1e731fc3837bec6
Ekaterina Aladyeva <aladyeva.e@wustl.edu>
2021-12-16 14:12:24 -0600

    small fix

--- SinkhornLinseed.R
+++ SinkhornLinseed.R
@@ -343,7 +343,7 @@ SinkhornLinseed <- R6Class(
             try_points[change_point] <- elem
             
             X <- self$V_row[try_points,] %*% t(self$R)
-            out <- tryCatch(solve(t(self$init_X),self$A)[,1], error = function(e) e)
+            out <- tryCatch(solve(t(self$X),self$A)[,1], error = function(e) e)
               if (!any(class(out) == "error")) {
                 
                   H <- X %*% self$R
