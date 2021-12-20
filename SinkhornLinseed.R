99d4da54ad6798dbe0fea75ac90d17a567461af8
Ekaterina Aladyeva <aladyeva.e@wustl.edu>
2021-12-20 15:38:59 -0600

    fix

--- SinkhornLinseed.R
+++ SinkhornLinseed.R
@@ -405,9 +405,6 @@ SinkhornLinseed <- R6Class(
                     new_init_Omega <- Omega
                     
                     total_init_error <- new_total_error
-
-                    
-                    break 
                   }
               }
             }
